b03bd65af464a6a51b0369e28bc72fae610275c5
Asingura Philip <asinguraphilip@gmail.com>
2024-07-27 23:48:38 +0300

    quiz html amd js

---
+++ quiz/quiz.js
@@ -0,0 +1,17 @@
+let answerA = document.querySelector("#a")
+let answerB = document.querySelector(".b")
+let answerC = document.querySelector(".c")
+let answerD = document.querySelector(".d")
+let form = document.getElementById("qnForm")
+
+console.log(form.length)
+console.log(answerA.checked)
+
+// function limit(n){
+//     let total = 0;
+//     for(let i=0;i<form.legth;i++){
+//         if(){
+
+//         }
+//     }
+// }
