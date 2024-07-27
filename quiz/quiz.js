2e8bca8de3c26b2ff3d8633c62b0258748b9645d
Asingura Philip <asinguraphilip@gmail.com>
2024-07-28 00:26:46 +0300

    added functions to limit number of options checked

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,17 +1,39 @@
 let answerA = document.querySelector("#a")
-let answerB = document.querySelector(".b")
-let answerC = document.querySelector(".c")
-let answerD = document.querySelector(".d")
+let answerB = document.querySelector("#b")
+let answerC = document.querySelector("#c")
+let answerD = document.querySelector("#d")
 let form = document.getElementById("qnForm")
 
-console.log(form.length)
-console.log(answerA.checked)
+console.log(form.a.checked)
+console.log(form.b.checked)
+console.log(form.c.checked)
+console.log(form.d.checked)
+// console.log(answerA.checked)
 
-// function limit(n){
-//     let total = 0;
-//     for(let i=0;i<form.legth;i++){
-//         if(){
 
-//         }
-//     }
-// }
+
+
+function limita(){
+    if((form.b.checked == true && form.d.checked == true) || (form.b.checked == true && form.c.checked == true)){
+        alert("you can only select two values")
+        form.a.checked = false
+    }
+}
+function limitb(){
+    if((form.a.checked == true && form.c.checked == true) || (form.a.checked == true && form.d.checked == true)){
+        alert("you can only select two values")
+        form.b.checked = false
+    }
+}
+function limitc(){
+    if((form.a.checked == true && form.b.checked == true) || (form.a.checked == true && form.d.checked == true)){
+        alert("you can only select two values")
+        form.c.checked = false
+    }
+}
+function limitd(){
+    if((form.a.checked == true && form.b.checked == true) || (form.a.checked == true && form.c.checked == true)){
+        alert("you can only select two values")
+        form.d.checked = false
+    }
+}
